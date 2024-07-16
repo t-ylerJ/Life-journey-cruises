671890df75d1f5cc457eea7952406988828c7ada
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-07-15 18:36:39 -0700

    Make changes to _index.jsx to fetch voyages data from database

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,9 @@
 import BigPicture from '../components/BigPicture'
 import Testimonials from '../components/Testimonials'
 import VoyageTiles from '../components/VoyageTiles'
+import sql from '../utils/sql.js';
+import {useLoaderData} from '@remix-run/react';
+import {json} from '@remix-run/react';
 
 export const meta = () => {
   return [
@@ -9,11 +12,24 @@ export const meta = () => {
   ]
 }
 
+
+export const loader = async () => {
+  try {
+    const data = await sql `SELECT * from voyages JOIN voyage_photos ON voyages.id = voyage_photos.voyage_id`;
+  return json(data);
+  } catch {
+    console.log('error in loading fleet data');
+  }
+};
+
+
 export default function Index() {
+  const data = useLoaderData();
+  console.log(data);
   return (
     <>
       <BigPicture />
-      <VoyageTiles />
+      <VoyageTiles voyages={data}/>
       <Testimonials />
     </>
   )
