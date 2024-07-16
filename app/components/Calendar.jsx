a10d5889a0b2a3156f88c09ad843d056353ee342
llu99 <l.lu90704@gmail.com>
2024-07-16 16:24:35 -0400

    calendar updates

--- app/components/Calendar.jsx
+++ app/components/Calendar.jsx
@@ -1,4 +1,36 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-DatePicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const selectabeDates = [
+  new Date('2025-06-01'),
+  new Date('2025-06-06'),
+  new Date('2025-06-11'),
+  new Date('2025-06-22'),
+  new Date('2025-07-03'),
+  new Date('2025-07-08'),
+  new Date('2025-07-13'),
+  new Date('2025-07-18'),
+  new Date('2025-07-23'),
+  new Date('2025-07-30'),
+  new Date('2025-08-06'),
+  new Date('2025-08-11'),
+  new Date('2025-08-16'),
+  new Date('2025-08-27'),
+  new Date('2025-09-07'),
+  new Date('2025-09-12'),
+  new Date('2025-09-17'),
+  new Date('2025-09-22'),
+  new Date('2025-09-27'),
+  new Date('2025-10-04'),
+];
+
+const isSelectable = (date: Date)
+
+
+
 const Calendar = () => {
+  const [startDate, setStartDate] = useState(null);
   return <div>Calendar</div>
 }
 
