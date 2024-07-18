e91d3a47b0e6f0dc09645d955773e0925abfe5f0
shaoyuchen3213 <chenshaoyu114@gmail.com>
2024-07-18 16:47:47 -0400

    fix ui for portdetail:

--- app/components/PortDetails.jsx
+++ app/components/PortDetails.jsx
@@ -1,51 +1,47 @@
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
-const PortDetails = ({ events, excursions, closeHandler,photo,description}) => {
+const PortDetails = ({ events, excursions, closeHandler, photo, description }) => {
   const closeModal = () => {
     closeHandler();
-    // Remove the extra closing curly brace
-  }
-    // if (!open) return null;
+  };
 
-    return (
-      <div className = "container md">
-        <div className="md:container  md:mx-auto mb-20 ">
-          <button onClick={closeModal} className = "float-right cursor-pointer">X</button>
-        </div>
-        <div>
-            <img src = {photo} alt = "City Look"/>
-             <p >{description}</p>
-        </div>
-        <div className = "grid grid-cols-2">
-        <div>
-          <h4 className="text-xl font-semibold text-gray-900 dark:text-white">
-            Event during the curise
-          </h4>
-          <div><ul >
-          {events ? events.map((event) =>
-            (
-
-                <li key={event.id}>{event}</li>
-
-            )
-          ) : null}
-          </ul>
-          </div>
-        </div>
-        <div>
-          <h4 className="text-xl font-semibold text-gray-900 dark:text-white">
-            Excursions at the port
-          </h4>
-          {excursions ? excursions.map(excursion =>
-            (
-              <ul key={excursion.id}>
-                <li>{excursion}</li>
-              </ul>
-            )
-          ) : null}
-        </div>
+  return (
+    <div className="container mx-auto p-4">
+      <div className="mb-1 flex justify-end">
+        <button onClick={closeModal} className="cursor-pointer p-2">
+          <IoIosCloseCircleOutline className="text-3xl text-gray-950" />
+        </button>
+      </div>
+      <div className="flex flex-col lg:flex-row items-start lg:items-center">
+        <div className="lg:flex-grow">
+          <img src={photo} alt="City Look" className="w-full lg:h-96 object-cover rounded-xl" />
+          <p className="mt-4 text-center leading-normal">{description}</p>
         </div>
       </div>
-    );
+      <div className="grid grid-cols-2 gap-4 text-justify mt-4">
+        <div>
+          <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
+            Event during the cruise
+          </h1>
+          <ul>
+            {events ? events.map((event, index) => (
+              <li key={index}>{event}</li>
+            )) : null}
+          </ul>
+        </div>
+        <div>
+          <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
+            Excursions at the port
+          </h1>
+          {excursions ? excursions.map((excursion, index) => (
+            <ul key={index}>
+              <li>{excursion}</li>
+            </ul>
+          )) : null}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PortDetails;
--- app/components/Ports.jsx
+++ app/components/Ports.jsx
@@ -6,8 +6,8 @@ const Ports = ({schedule, clickHandler}) => {
   return <div>
     {schedule.map((item) => {
       return (
-        <div  key={item.day} className="text-left p-3 m-3 hover:bg-blue-300 border-b-2" type="button">
-          <button data-modal-target="default-modal" data-modal-show="default-modal" className="text-2xl font-bold b " value={item.day}  onClick={handClickEvent} type="button">Day  {item.day}: {item.portname} </button>
+        <div  key={item.day} className="text-left p-3 hover:bg-blue-300 border-b-2" type="button">
+          <button data-modal-target="default-modal" data-modal-show="default-modal" className="font-bold b " value={item.day}  onClick={handClickEvent} type="button">Day  {item.day}: {item.portname} </button>
         </div>
       )
     })}
