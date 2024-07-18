8acf17565411ef80b1eefcb60c51052db9e9d0b7
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-07-18 00:11:50 -0700

    Re-implement big picture feature

--- app/components/BigPicture.jsx
+++ app/components/BigPicture.jsx
@@ -1,108 +1,123 @@
+import { useEffect, useState } from 'react';
+
 const BigPicture = () => {
+
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  useEffect(() => {
+    const slides = ["#slide1", "#slide2", "#slide3"];
+    const totalSlides = slides.length;
+
+    const nextSlide = () => {
+      setCurrentSlide((prev) => (prev % totalSlides) + 1);
+    };
+
+    const interval = setInterval(nextSlide, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
+  <div className="carousel w-full">
 
-<div className="carousel w-auto">
-  <div id="slide1" className="carousel-item relative w-full">
-    <div id="item1" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="celebrate.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide7" className="btn btn-circle">❮</a>
+    <div id="slide1" className={`flex carousel-item relative w-full ${currentSlide === 1 ? 'block' : 'hidden'}`}>
+      <div  className="carousel-item w-[33.3%]">
+        <video autoPlay loop muted >
+        <source src="bigpicture/854397-hd_1280_720_30fps.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div  className="carousel-item w-[33.3%]">
+        <video autoPlay loop muted >
+        <source src="bigpicture/2096548-hd_1920_1080_30fps (2).mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/CELEBRATE friends.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+      <a href="#slide3" className="btn btn-circle">❮</a>
       <a href="#slide2" className="btn btn-circle">❯</a>
+    </div> */}
     </div>
-  </div>
 
-  <div id="slide2" className="carousel-item relative w-full">
-    <div id="item2" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="party.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+
+    <div id="slide2" className={`flex carousel-item relative w-full ${currentSlide === 2 ? 'block' : 'hidden'}`}>
+
+      <div  className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/fun cruises daycruise.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div  className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/dream.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/4782135-uhd_3840_2160_25fps.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide1" className="btn btn-circle">❮</a>
       <a href="#slide3" className="btn btn-circle">❯</a>
+    </div> */}
     </div>
-  </div>
 
-  <div id="slide3" className="carousel-item relative w-full">
-    <div id="item3" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="adventure_awaits.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+
+    <div id="slide3" className={`flex carousel-item relative w-full ${currentSlide === 3 ? 'block' : 'hidden'}`}>
+      <div  className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/3094026-uhd_3840_2160_30fps.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div  className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/ADVENTURE awaitsmp4.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+      <div className="carousel-item w-[33.3%] ">
+        <video autoPlay loop muted >
+        <source src="bigpicture/PARTY.mp4" type="video/mp4" />
+        <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
+        Your browser does not support the video tag.
+        </video>
+      </div>
+
+    {/* <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
       <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-
-
-  <div id="slide4" className="carousel-item relative w-full">
-    <div id="item4" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="dream.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide3" className="btn btn-circle">❮</a>
-      <a href="#slide5" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-
-  <div id="slide5" className="carousel-item relative w-full">
-    <div id="item5" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="enjoy.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide4" className="btn btn-circle">❮</a>
-      <a href="#slide6" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-
-  <div id="slide6" className="carousel-item relative w-full">
-    <div id="item6" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="discover_beauty.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide5" className="btn btn-circle">❮</a>
-      <a href="#slide7" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-
-  <div id="slide7" className="carousel-item relative w-full">
-    <div id="ite7" className="carousel-item w-full">
-      <video autoPlay loop muted className="w-full">
-      <source src="FUN_CRUISES.mp4" type="video/mp4" />
-      <track kind="captions" srcLang="en" src="/videos/captions1.vtt" />
-      Your browser does not support the video tag.
-      </video>
-    </div>
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide6" className="btn btn-circle">❮</a>
       <a href="#slide1" className="btn btn-circle">❯</a>
+    </div> */}
     </div>
   </div>
-</div>
-
-  )
-}
+)}
 
 export default BigPicture;
