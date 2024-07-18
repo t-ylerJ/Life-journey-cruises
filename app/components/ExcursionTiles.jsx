b7a995e157606603b43f1ad4c8cca4e5cd940f9c
Liru Zhao <zlrhhj@hotmail.com>
2024-07-18 13:36:37 -0700

    made the page responsive

--- app/components/ExcursionTiles.jsx
+++ app/components/ExcursionTiles.jsx
@@ -42,15 +42,15 @@ const ExcursionTiles = ({dailyExcursions}) => {
    }
   return (
 
-      <div className="flex bg-gray-100 flex-col justify-center items-center overflow-hidden p-10 m-10" >
+      <div className="flex bg-gray-100 flex-col justify-center items-center overflow-hidden md:p-10 p-4 md:m-10 m-2"  >
         {dailyExcursions && dailyExcursions.map((daily) => {
           return(
             <div key={daily.day} className=" w-full p-0 m-0 rounded-lg shadow-lg border-overflow-hidden border-2 border-inherit bg-white justify-around text-center mb-4"  >
               <div className="w-full pt-5" >
-                <h1 className="text-3xl font-bold" >Day: {daily.day}</h1>
-                <h2 className="text-3xl p-5">Port: {daily.portname}</h2>
+                <h1 className="text-2xl md:text-3xl font-bold" >Day: {daily.day}</h1>
+                <h2 className="text-2xl md:text-3xl p-5 md:p-2">Port: {daily.portname}</h2>
               </div>
-              <div className="flex justify-around">
+              <div className="flex flex-wrap justify-start xl:justify-around" >
               {
                 daily.excursions && daily.excursions.map((excursion) => {
                   return (
@@ -58,9 +58,9 @@ const ExcursionTiles = ({dailyExcursions}) => {
                       className={
                         clickedBoxes.includes(parseInt(excursion.id))
                         ?
-                        "w-[17%] h-96 flex items-left flex-col bg-blue-600 text-white border-2 p-5 mb-10 rounded-xl"
+                        "w-[50%] md:w-[32%] xl:w-[17%] h-60 md:h-96 flex items-left flex-col bg-primary text-white border-2 p-3 md:p-5 mb-2 md:mb-10 rounded-xl"
                         :
-                        "w-[17%] h-96 flex items-left flex-col bg-gray-100 border-2 p-5 mb-10 rounded-xl"
+                        "w-[50%] md:w-[32%] xl:w-[17%] h-60 md:h-96 flex items-left flex-col bg-gray-100 border-2 p-3 md:p-5 mb-2 md:mb-10 rounded-xl"
                       }
                       onClick={()=>{
                         boxOnClick(excursion.id);
@@ -68,12 +68,12 @@ const ExcursionTiles = ({dailyExcursions}) => {
                       value={excursion.id}
                       key={excursion.id}
                     >
-                      <h3 className="text-left text-3xl">{excursion.name}</h3>
-                      <span className="text-l pt-2 pb-2">${excursion.price}</span>
-                      <img className="w-[100%] h-56 max-h-56 min-h-48 rounded object-cover" src={excursion.image} alt={excursion.name} onClick={(e)=>{
+                      <h3 className="text-left text-lg md:text-3xl">{excursion.name}</h3>
+                      <span className="text-sm md:text-l pt-1 md:pt-2  pb-1 md:pb-2">${excursion.price}</span>
+                      <img className="w-full h-32 md:h-56 max-h-32 md:max-h-56 min-h-24 md:min-h-48 rounded object-cover" src={excursion.image} alt={excursion.name} onClick={(e)=>{
                         boxOnClick(excursion.id);
                       }} />
-                      <span className="pb-5 pt-5">{adjustTime(excursion.time)}</span>
+                      <span className="pb-1 md:pb-5 pt-1 md:pt-5">{adjustTime(excursion.time)}</span>
 
                     </button>
                   )
