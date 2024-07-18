4821c1aa68587ad7d8b0db172fffa5a7c44622c0
Tanner <tanjac00@yahoo.com>
2024-07-18 12:05:35 -0600

    Gets passed info from previous page and passes info to summary page

--- app/components/PaymentSummary.jsx
+++ app/components/PaymentSummary.jsx
@@ -1,8 +1,39 @@
+import React from 'react';
+
 const PaymentSummary = () => {
-
-  return (<div>
-
-  </div>)
+  const [payInfo, setPayInfo] = React.useState({});
+    React.useEffect(() => {
+      setPayInfo(JSON.parse(localStorage.getItem('payInfo')));
+    }, []);
+  console.log(payInfo);
+  const payKeys = Object.keys(payInfo);
+  if (payInfo.guests) {
+    return (<div>
+      <h2>Guests:</h2>
+      {
+        payInfo.guests.map((item, index) => {
+          return (
+            <div key={`guest${index + 1}`}>
+              <h3>Guest {index + 1}</h3>
+              <p>Name: {item.first_name + item.last_name}</p>
+              <p>Passport #: {item.passport}</p>
+            </div>
+          )
+        })
+      }
+      <h2>Payment Info:</h2>
+      {
+        payKeys.map((pKey) => {
+          if (pKey !== 'guests') {
+            return (<div key={pKey}>
+              <p>{pKey}:</p>
+              <p>{payInfo[pKey]}</p>
+            </div>)
+          }
+        })
+      }
+    </div>)
+  }
 }
 
 export default PaymentSummary;
