7cf76669bddfdc0442d0c61979e9f450ecd07eb1
shaoyuchen3213 <chenshaoyu114@gmail.com>
2024-07-18 18:39:50 -0400

    change button styling

--- app/components/Ports.jsx
+++ app/components/Ports.jsx
@@ -3,10 +3,10 @@ const Ports = ({schedule, clickHandler}) => {
     e.preventDefault();
     clickHandler(e.target.value);
   }
-  return <div className="w-3/5">
+  return <div className="w-2/5">
     {schedule.map((item) => {
       return (
-        <div  key={item.day} className="text-left p-3 hover:bg-blue-300 border-b-2" type="button">
+        <div  key={item.day} className="rounded-full text-left p-3 pb-8 hover:bg-blue-300 border-b-2" type="button">
           <button data-modal-target="default-modal" data-modal-show="default-modal" className="font-bold b " value={item.day}  onClick={handClickEvent} type="button">Day  {item.day}: {item.portname} </button>
         </div>
       )
