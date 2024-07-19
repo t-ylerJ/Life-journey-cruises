9211b7a9977455a2283a6aac03e4d56099e95734
Jason Ullrich <jasonullrich@gmail.com>
2024-07-19 16:28:03 -0700

    Fix map

--- app/components/Ports.jsx
+++ app/components/Ports.jsx
@@ -1,19 +1,55 @@
-const Ports = ({schedule, clickHandler}) => {
+const Ports = ({ schedule, clickHandler, setHighlightedPort, hoveredPort }) => {
   const handClickEvent = (e) => {
-    e.preventDefault();
-    clickHandler(e.target.value);
+    e.preventDefault()
+    clickHandler(e.target.value)
   }
-  return <div className="w-full">
-    {schedule.map((item) => {
-      const hoverClass = item.day === 1 ?  'hover:bg-secondary/30' :'hover:bg-blue-300/30';
-      return (
-        <div  key={item.day} className={`text-left divide-y cursor-pointer p-3 pb-8 ${hoverClass} border-b-2`} type="button" onClick={()=>{clickHandler(item.day)}}>
-          <button data-modal-target="default-modal" data-modal-show="default-modal" className="font-bold b " value={item.day}  onClick={handClickEvent} type="button">Day  {item.day}: {item.portname} </button>
-        </div>
-      )
-    })}
-  </div>
+  // Function for handling mouse enter event
+  function handleMouseEnter(portName) {
+    // setHighlightedPort(portName);
+    hoveredPort.current = portName
+    console.log('cactus')
+    console.log('hoveredPort: ', hoveredPort.current)
+  }
+
+  // Function for handling mouse leave event
+  function handleMouseLeave() {
+    // setHighlightedPort('');
+    hoveredPort.current = null
+  }
+
+  return (
+    <div className="w-full">
+      {schedule.map((item, index) => {
+        const hoverClass =
+          item.day === 1 ? 'hover:bg-orange-300' : 'hover:bg-blue-300'
+        return (
+          <div
+            key={item.day}
+            className={`text-3xl text-left cursor-pointer p-3 pb-8 ${hoverClass} border-b-2`}
+            type="button"
+            onClick={() => {
+              clickHandler(item.day)
+            }}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            <div
+              data-modal-target="default-modal"
+              data-modal-show="default-modal"
+              className="font-bold b "
+              value={item.day}
+              onClick={() => {
+                clickHandler(item.day)
+              }}
+              type="button"
+            >
+              Day {item.day}: {item.portname}{' '}
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Ports
-
