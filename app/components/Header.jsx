b5185fa5ebc94738ba0b5dfecefbfe19ace2b343
Jason Ullrich <jasonullrich@gmail.com>
2024-07-19 15:27:44 -0700

    Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react';
-import { Link, Form, useNavigate } from '@remix-run/react';
+import { useState } from 'react'
+import { Link, Form, useNavigate } from '@remix-run/react'
 
 const Header = ({ user }) => {
-  const [currentPage, setCurrentPage] = useState(0);
-  const navigate = useNavigate();
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0)
+  const navigate = useNavigate()
+  const [dropdownOpen, setDropdownOpen] = useState(false)
 
   const handleHomeClick = () => {
-    setCurrentPage(0);
-    navigate('/');
-  };
+    setCurrentPage(0)
+    navigate('/')
+  }
 
   return (
-    <div style={{ backgroundImage: "url('/HeaderBG.svg')"}} className="flex justify-between items-center p-4 bg-white">
+    <div
+      // style={{ backgroundImage: "url('/HeaderBG.svg')" }}
 
+      className="flex justify-between items-center p-4 py-8 bg-white relative"
+    >
+      <img src="/LogoTop.svg" style={{ width: '220px' }} />
 
-      <nav className="flex-1 flex justify-center space-x-12">
+      <div
+        className="absolute inset-0"
+        style={{
+          background: 'url(/wave.svg)',
+          backgroundRepeat: 'space no-repeat',
+          backgroundPosition: '0% 100%',
+          backgroundSize: '1.19%',
+        }}
+      ></div>
+      <img
+        className="absolute left-3 bottom-2"
+        src="/LogoBottom.svg"
+        style={{ width: '220px' }}
+      />
+      <nav className="flex-1 flex justify-center space-x-12 z-30 -translate-y-4">
         <Link
           onClick={handleHomeClick}
           to="/"
-          className="text-lg pr-20 font-semibold text-black no-underline"
+          className="text-lg pr-20 font-semibold text-black no-underline hover:underline"
         >
           Home
         </Link>
         <Link
           to="/about"
-          className="text-lg pl-20 pr-40 font-semibold text-black no-underline"
+          className="text-lg pl-20 pr-40 font-semibold text-black no-underline hover:underline"
         >
           About
         </Link>
@@ -45,33 +63,37 @@ const Header = ({ user }) => {
             <img
               src="/avatar.svg"
               alt="Avatar"
-              className="p-2 w-20 h-20 cursor-pointer"
+              className="w-12 h-12 cursor-pointer"
               onMouseEnter={() => setDropdownOpen(true)}
             />
             {dropdownOpen && (
               <div
-                className="absolute right-0 mt-2 w-20 bg-white rounded-md shadow-lg py-2 z-50"
+                className="absolute right-0 mt-2 w-20 bg-white rounded-md shadow-lg z-50 divide-y divide-y-black overflow-hidden flex flex-col"
                 onMouseEnter={() => setDropdownOpen(true)}
                 onMouseLeave={() => setDropdownOpen(false)}
               >
-                <Form method='post'>
-                  <button className="b-2">
-                    Logout
-                  </button>
+                <Form method="post">
+                  <button className="b-2 hover:bg-gray-200 p-3">Logout</button>
                 </Form>
-                <Link to="/account" className="b-2">Account</Link>
+                <Link to="/account" className="b-2 hover:bg-gray-200 p-3">
+                  Account
+                </Link>
               </div>
             )}
           </div>
         ) : (
           <div className="flex space-x-4">
-            <Link to="/login" className="btn bg-secondary text-white">Login</Link>
-            <Link to="/signup" className="btn bg-secondary text-white">Signup</Link>
+            <Link to="/login" className="btn btn-secondary">
+              Login
+            </Link>
+            <Link to="/signup" className="btn btn-secondary">
+              Signup
+            </Link>
           </div>
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
